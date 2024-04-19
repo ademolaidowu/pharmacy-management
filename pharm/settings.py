9d7511f0195f0f312da2032d1b9b33cb85c6f579
ademolaidowu <wunmidev@gmail.com>
2024-04-19 01:29:39 +0100

    Updated settings

--- pharm/settings.py
+++ pharm/settings.py
@@ -25,13 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ui1u$88tg97g-()a(^hcv5mn#jzcu^3yb1n=!s7kn-!t25932n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['pharmacymanage.herokuapp.com']
-ALLOWED_HOSTS = [
-    'pharmacy-management-system2.herokuapp.com',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -170,8 +166,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_USE_TLS= True
-EMAIL_HOST_USER=''
-EMAIL_HOST_PASSWORD=''
+# EMAIL_HOST_USER
+# EMAIL_HOST_PASSWORD
 
 
 
